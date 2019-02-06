0a1e82d6368165cf9346e2049d7f76e2632b186d
Mark Eaton <mark@LAdmins-MacBook-Pro.local>
2019-02-06 15:45:10 -0600

    initial commit - starter project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp_core_test_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
